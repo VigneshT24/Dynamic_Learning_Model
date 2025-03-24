b126211ed8ba620e0963c22b00f88dfaec253df2
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-03-24 16:53:14 -0700

    Update DLM.py

--- DLM.py
+++ DLM.py
@@ -1,8 +1,4 @@
 # Dynamic-Learning Model that learns how to respond to questions by learning from user input/expectations
-# STEPS TO DEVELOP:
-#   * allow user to ask an input
-#   * if the input is not recognizable (not stored in stored_data.txt), then ask user what the response should be, store the question-answer pair in file
-#   * keep doing this until a recognizable query comes up, and in that situation, give the response stored in key-value pair
 import difflib
 class DLM:
     __filename = "stored_data.txt"
@@ -16,7 +12,11 @@ class DLM:
 
     # private helper called inside public method "ask" to simply add question/answer pair in specific text file
     def __learn(self, query, expectation):
-        self.__file.write(query + ">>" + expectation + "\n")
+        with open(self.__filename, "r") as file:
+            for line in file:
+                # don't do anything if pair already exists in database
+                if (query + ">>" + expectation) == line: return 
+        self.__file.write("\n" + query + ">>" + expectation)
 
     # only method developer/user can use to train DLM or get answers to known queries
     def ask(self):
@@ -24,10 +24,11 @@ class DLM:
         with open(self.__filename, "r") as file: # go through database to see if question fuzzily matches with anything, if so, answer the question, else, learn the question
             for line in file:
                 match = difflib.SequenceMatcher(None, self.__query, line.strip().split(">>")[0])
-                if match.ratio() > 0.75: # returns a ratio representing how much the current question matches a specific question in database
-                    print(line.split(">>", 1)[1].strip()) # if 75% match, return the answer
-                    # ASK IF THIS IS THE ANSWER THEY ARE LOOKING FOR!
-                    return
+                if match.ratio() > 0.9: # returns a ratio representing how much the current question matches a specific question in database
+                    print(line.split(">>", 1)[1].strip()) # if 75% match, give the answer
+                    self.__expectation = input("Is this what you expected (Y/N): ")
+                    if self.__expectation.lower() == "y": return
+                    break
         self.__expectation = input("What was the expected response (training mode): ") # train DLM
         self.__learn(self.__query, self.__expectation) # learn this new question and answer pair and add to stored_data.txt
         print("I learned something new!") # confirmation that it went through the whole process
