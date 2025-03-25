cbb16548320d8c29084a3ab4e2a8d0b05ab01229
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-03-25 09:43:22 -0700

    Update DLM.py

--- DLM.py
+++ DLM.py
@@ -5,13 +5,15 @@ class DLM:
     __file = None
     __query = None
     __expectation = None
+    __satisfied = False
 
     # private helper that checks to see if given query already exists in the database (avoids redundant data)
     def __data_exists(self):
-        with open(self.__filename, "r") as file:
+        with open(self.__filename, "r") as file:  # go through stored_data.txt (O(n)) to see if similar data exists
             for line in file:
-                # don't do anything if pair already exists in database
-                if self.__query == line.strip().split(">>")[0]: return True
+                match = difflib.SequenceMatcher(None, self.__query, line.strip().split(">>")[0])
+                if match.ratio() > 0.90:  # there is a similar question already, don't add a new one if not necessary
+                    return True
         return False
 
     # private helper that handles the learning aspect of the bot; if Q/A pair doesn't exist in database, add it there
@@ -28,14 +30,18 @@ class DLM:
             with open(self.__filename, "r") as file: # go through database to see if question fuzzily matches with anything, if so, answer the question, else, learn the question
                 for line in file:
                     match = difflib.SequenceMatcher(None, self.__query, line.strip().split(">>")[0])
-                    if match.ratio() > 0.85: # returns a ratio representing how much the current question matches a specific question in database
-                        print(f"\n{'\033[34m'}" + line.split(">>", 1)[1].strip() + f"{'\033[0m'}\n") # if 85% match, give the answer
+                    if match.ratio() > 0.90: # returns a ratio representing how much the current question matches a specific question in database
+                        print(f"\n{'\033[34m'}" + line.split(">>", 1)[1].strip() + f"{'\033[0m'}\n") # if 90% match, give the answer
                         self.__expectation = input("Is this what you expected (Y/N): ")
+                        while not self.__expectation:
+                            self.__expectation = input("Empty input is not acceptable. Is this what you expected (Y/N): ")
                         if self.__expectation.lower() == "y":
+                            self.__satisfied = True
                             print("Great!")
                             return
+                        self.__satisfied = False
                         break
-        if not self.__data_exists():
+        if not self.__data_exists() or not self.__satisfied:
             self.__expectation = input("I don't know the answer. What was the expected response (training mode): ") # train DLM
             while not self.__expectation:
                 print("Nothing learnt. Moving on.")
