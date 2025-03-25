2475bfb6c781788661626cf7dc0201bbdd5027c5
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-03-24 17:04:45 -0700

    Update DLM.py

--- DLM.py
+++ DLM.py
@@ -15,20 +15,22 @@ class DLM:
         with open(self.__filename, "r") as file:
             for line in file:
                 # don't do anything if pair already exists in database
-                if (query + ">>" + expectation) == line: return 
+                if (query + ">>" + expectation) == line: return
         self.__file.write("\n" + query + ">>" + expectation)
 
     # only method developer/user can use to train DLM or get answers to known queries
     def ask(self):
-        self.__query = input("Query: ")
+        self.__query = input("DML Bot here, ask away: ")
         with open(self.__filename, "r") as file: # go through database to see if question fuzzily matches with anything, if so, answer the question, else, learn the question
             for line in file:
                 match = difflib.SequenceMatcher(None, self.__query, line.strip().split(">>")[0])
-                if match.ratio() > 0.9: # returns a ratio representing how much the current question matches a specific question in database
-                    print(line.split(">>", 1)[1].strip()) # if 75% match, give the answer
+                if match.ratio() > 0.85: # returns a ratio representing how much the current question matches a specific question in database
+                    print(f"\n{'\033[34m'}" + line.split(">>", 1)[1].strip() + f"{'\033[0m'}\n") # if 85% match, give the answer
                     self.__expectation = input("Is this what you expected (Y/N): ")
-                    if self.__expectation.lower() == "y": return
+                    if self.__expectation.lower() == "y":
+                        print("Great!")
+                        return
                     break
-        self.__expectation = input("What was the expected response (training mode): ") # train DLM
+        self.__expectation = input("I don't know the answer. What was the expected response (training mode): ") # train DLM
         self.__learn(self.__query, self.__expectation) # learn this new question and answer pair and add to stored_data.txt
         print("I learned something new!") # confirmation that it went through the whole process
