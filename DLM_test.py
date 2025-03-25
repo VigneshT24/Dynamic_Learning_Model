9e9eecb9039e4af082bf8870935b1310a1af5d14
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-03-24 20:34:08 -0700

    Update DLM_test.py

--- DLM_test.py
+++ DLM_test.py
@@ -2,9 +2,13 @@ from DLM import DLM
 
 canContinue = True
 
+print(f"{'\033[31m'}Welcome to Dynamic Learning Model Bot (DLM Bot){'\033[0m'}")
+print(f"{'\033[31m'}DLM Bot gets smarter for every query asked because it either knows it or learns it{'\033[0m'}")
+userChoice = input("\nDo you want to solely train DLM or ask DLM with questions (type 'train' or 'ask'): ")
+
+bot = DLM()
+
 while canContinue:
-    bot = DLM()
-    bot.ask()
+    bot.ask(userChoice == "train")
     choice = input("Continue (Y/N): ")
-    if (choice.lower() != "y"): canContinue = False
-    else: canContinue = True
+    canContinue = choice.lower() == "y"
