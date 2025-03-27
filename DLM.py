c4a6fc8aee67ca37a49733dada9d441a4493495b
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-03-26 17:49:53 -0700

    Update DLM.py

--- DLM.py
+++ DLM.py
@@ -8,72 +8,106 @@ class DLM:
     __query = None # user-inputted query
     __expectation = None # user-inputted expected answer to query
 
-    # words to be filtered from user input for better accuracy and less distractions
-    __filler_words = [
-        # Articles & Determiners
-        "a", "an", "the", "some", "any", "each", "every", "either", "neither", "this", "that", "these", "those",
+    # personalized responses to let the user know that the bot doesn't know the answer
+    __fallback_responses = [
+        "Hmm, that's a great question! I don't have the answer right now, but I'm always learning!",
+        "I'm still training my brain on that topic. Check back soon!",
+        "Oops! That one's not in my database yet, but I’ll work on adding it!",
+        "You got me this time! But I'm taking notes for the future!",
+        "That's a tough one! Let me do some more learning and get back to you.",
+        "I don't have the answer just yet, but I bet it’s out there somewhere!",
+        "Hmm... I’ll have to hit the books for that one!",
+        "I haven't learned that yet, but I'm constantly improving!",
+        "You just stumped me! But no worries, I’m always evolving!",
+        "That’s outside my knowledge base for now, but I appreciate the challenge!",
+        "I wish I had the answer! But hey, this means there's more for me to learn!",
+        "I’m not sure about that one. Try another query.",
+        "Hmm, I don’t have an answer yet. But if you find out, let me know!",
+        "Still learning this one! Sorry...",
+        "I don’t have that in my knowledge bank yet, but I’m working on it!"
+    ]
 
-        # Pronouns
+    # words to be filtered from user input for better accuracy and fewer distractions
+    __filler_words = [
+
+        # Articles & Determiners (Words that don't add meaning to sentence)
+        "a", "an", "the", "some", "any", "each", "every", "either", "neither", "this", "that", "these", "those",
+        "certain", "another", "such", "whatsoever", "whichever", "whomever", "whatever", "all"
+
+        # Pronouns (General pronouns that don’t change meaning)
         "i", "me", "my", "mine", "myself", "you", "your", "yours", "yourself", "he", "him", "his", "himself",
         "she", "her", "hers", "herself", "it", "its", "itself", "we", "us", "our", "ours", "ourselves",
         "they", "them", "their", "theirs", "themselves", "who", "whom", "whose", "which", "that",
+        "someone", "somebody", "anyone", "anybody", "everyone", "everybody", "nobody", "people", "person",
+        "whoever", "wherever", "whenever", "whosoever", "others", "oneself",
 
-        # Auxiliary Verbs (Helping Verbs)
-        "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having",
-        "do", "does", "did", "doing", "shall", "should", "will", "would", "can", "could", "may", "might", "must",
-
-        # Conjunctions
+        # Auxiliary (Helping) Verbs (Do not contribute meaning)
+        "get", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "best"
+        "do", "does", "did", "doing", "shall", "should", "will", "would", "can", "could", "may", "might", "must", "bad"
+        "dare", "need", "used", "shalln't", "should've", "would've", "could've", "must've", "might've", "mustn't", "good"
+                                                                                                                   
+        # Conjunctions (Connectors that do not change meaning)
         "and", "but", "or", "nor", "so", "for", "yet", "although", "though", "because", "since", "unless",
-        "while", "whereas", "either", "neither", "both", "whether", "not", "if",
+        "while", "whereas", "either", "neither", "both", "whether", "not", "if", "even if", "even though", "common"
+        "as long as", "provided that", "whereas", "therefore", "thus", "hence", "meanwhile", "besides", "furthermore",
 
-        # Prepositions
+        # Prepositions (Location/Relation words that are often unnecessary)
         "about", "above", "across", "after", "against", "along", "among", "around", "as", "at",
-        "before", "behind", "below", "beneath", "beside", "between", "beyond", "by",
+        "before", "behind", "below", "beneath", "beside", "between", "beyond", "by", "low", "high"
         "despite", "down", "during", "except", "for", "from", "in", "inside", "into",
         "like", "near", "of", "off", "on", "onto", "out", "outside", "over", "past",
         "since", "through", "throughout", "till", "to", "toward", "under", "underneath",
-        "until", "up", "upon", "with", "within", "without",
+        "until", "up", "upon", "with", "within", "without", "aside from", "concerning", "regarding",
 
-        # Common Adverbs (that don’t add meaning)
-        "again", "already", "also", "always", "ever", "never", "just", "now", "often",
+        # Common Adverbs (Time words and intensity words that add fluff)
+        "way", "ways", "again", "already", "also", "always", "ever", "never", "just", "now", "often",
         "once", "only", "quite", "rather", "really", "seldom", "sometimes", "soon",
-        "still", "then", "there", "therefore", "thus", "too", "very", "well",
+        "still", "then", "there", "therefore", "thus", "too", "very", "well", "anytime",
+        "hardly", "barely", "scarcely", "seriously", "truly", "frankly", "honestly", "basically", "literally",
+        "definitely", "obviously", "surely", "likely", "probably", "certainly", "clearly", "undoubtedly",
 
-        # Question Words
+        # Question Words (Words that do not impact search meaning)
         "what", "when", "where", "which", "who", "whom", "whose", "why", "how",
+        "whichever", "whomever", "whenever", "wherever", "whosoever", "however", "whence",
 
-        # Informal/Common Fillers
+        # Informal/Common Fillers (Spoken language fillers)
         "gonna", "wanna", "gotta", "lemme", "dunno", "kinda", "sorta", "aint", "ya", "yeah", "nah",
+        "um", "uh", "hmm", "huh", "mmm", "uhh", "ahh", "err", "ugh", "tsk", "like", "okay", "ok", "alright",
+        "yo", "bruh", "dude", "bro", "sis", "mate", "fam", "nah", "yup", "nope", "welp",
 
         # Verbs Commonly Used in Questions (but don’t change meaning)
-        "do", "does", "did", "can", "could", "should", "shall", "will", "would", "may", "might", "must",
-
-        # Additional Filler Phrases
+        "do", "does", "did", "can", "could", "should", "shall", "will", "would", "may", "might", "must", "use"
         "tell", "please", "say", "let", "know", "consider", "find", "show", "explain", "define", "describe",
-        "list", "give", "provide", "help", "make", "see", "like", "mean",
+        "list", "give", "provide", "help", "make", "see", "mean", "understand", "point out", "stay", "look", "care"
 
-        # Contracted Forms (Common in Speech & Casual Writing)
+        # Contracted Forms (Casual writing contractions)
         "i'd", "i'll", "i'm", "i've", "you'd", "you'll", "you're", "you've", "he'd", "he'll", "he's",
         "she'd", "she'll", "she's", "we'd", "we'll", "we're", "we've", "they'd", "they'll", "they're", "they've",
         "it's", "that's", "who's", "what's", "where's", "when's", "why's", "how's", "there's", "here's", "let's",
 
-        # Miscellaneous Fillers
+        # Conversational Fillers (Unnecessary words in casual speech)
         "actually", "basically", "seriously", "literally", "obviously", "honestly", "frankly", "clearly",
-        "apparently", "probably", "definitely", "certainly", "mostly", "mainly", "typically", "essentially",
+        "apparently", "probably", "definitely", "certainly", "most", "mostly", "mainly", "typically", "essentially",
         "generally", "approximately", "virtually", "kind", "sort", "type", "whatever", "however",
-
-        # Excess Words That Don’t Change Sentence Meaning
-        "thing", "stuff", "someone", "somebody", "anyone", "anybody", "everyone", "everybody", "nobody",
-        "people", "person", "something", "anything", "everything", "nothing",
-
-        # Placeholder & Non-Descriptive Words
-        "thingy", "whatchamacallit", "doohickey", "thingamajig", "thingamabob",
-
-        # Conversational Phrases That Don’t Add Meaning
         "you know", "i mean", "you see", "by the way", "sort of", "kind of", "more or less",
         "as far as i know", "in my opinion", "to be honest", "to be fair", "just saying",
-        "at the end of the day", "if you ask me", "truth be told", "the fact is", "long story short"
+        "at the end of the day", "if you ask me", "truth be told", "the fact is", "long story short",
+
+        # Internet Slang, Misspellings, and Shortcuts
+        "lol", "lmao", "rofl", "omg", "idk", "fyi", "btw", "imo", "smh", "afk", "ttyl", "brb",
+        "thx", "pls", "ppl", "u", "ur", "r", "cuz", "coz", "cause", "gimme", "lemme", "wassup", "sup",
+
+        # Placeholder & Non-Descriptive Words
+        "thing", "stuff", "thingy", "whatchamacallit", "doohickey", "thingamajig", "thingamabob",
+
+        # Words That Don’t Add Meaning
+        "important", "necessary", "specific", "certain", "particular", "special", "exactly", "precisely",
+        "recently", "currently", "today", "tomorrow", "yesterday", "soon", "later", "eventually", "sometime",
+
+        # Overused Transitions
+        "so", "then", "therefore", "thus", "anyway", "besides", "moreover", "furthermore", "meanwhile"
     ]
+
     def __learn(self, query, expectation):
         """ Stores the new query and expectation pair in stored_data.txt """
         with open(self.__filename, "a") as file:
@@ -108,8 +142,9 @@ class DLM:
         # Join the remaining words back into a string
         return " ".join(filtered_words)
 
-    def ask(self):
+    def ask(self, trainingMode):
         """ Main method in which the user is able to ask any query and DLM will either answer it or learn it """
+        print("\nTRAINING MODE") if (trainingMode == True) else print("\nCOMMERCIAL MODE")
         self.__query = input("DLM Bot here, ask away: ")
 
         # storing the user-query (filtered and lower-case)
@@ -121,8 +156,8 @@ class DLM:
                 stored_question = line.strip().split(">>")[0].lower()
                 similarity = difflib.SequenceMatcher(None, stored_question, filtered_query).ratio()
 
-                # only accept a match if similarity is 87% or more
-                if similarity >= 0.87:
+                # only accept a match if similarity is 80% or more
+                if similarity >= 0.80:
                     print(f"\n{'\033[34m'}" + line.split(">>", 1)[1].strip() + f"{'\033[0m'}\n")
                     self.__expectation = input("Is this what you expected (Y/N): ")
 
@@ -139,11 +174,14 @@ class DLM:
             print(str(incompleteness))
             return
 
-        self.__expectation = input("I'm not sure. What was the expected response (training mode): ") # train DLM
+        if (trainingMode):
+            self.__expectation = input("I'm not sure. What was the expected response (training mode): ") # train DLM
 
-        while not self.__expectation:
-            print("Nothing learnt. Moving on.")
-            return
+            while not self.__expectation:
+                print("Nothing learnt. Moving on.")
+                return
 
-        self.__learn(filtered_query, self.__expectation) # learn this new question and answer pair and add to stored_data.txt
-        print("I learned something new!") # confirmation that it went through the whole process
+            self.__learn(filtered_query, self.__expectation) # learn this new question and answer pair and add to stored_data.txt
+            print("I learned something new!") # confirmation that it went through the whole process
+        else:
+            print(random.choice(self.__fallback_responses))
