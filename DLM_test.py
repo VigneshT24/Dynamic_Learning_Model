365d65af6de0fcf1375038829e1c6fc31a1e2fe4
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-03-28 11:00:35 -0700

    Update DLM_test.py

--- DLM_test.py
+++ DLM_test.py
@@ -4,6 +4,7 @@ canContinue = True
 
 print(f"{'\033[31m'}Welcome to Dynamic Learning Model Bot (DLM Bot). This bot can be trained for any purposes.{'\033[0m'}")
 print(f"{'\033[31m'}DLM Bot gets smarter for every query asked because it either knows it or learns it for next time{'\033[0m'}")
+print(f"{'\033[31m'}NOTICE: DLM Bot may sometimes misinterpret input or provide inaccurate responses. Please verify important information independently.{'\033[0m'}")
 userChoice = input("\nThere are two options: Train the DLM Bot (type 'T') or use it as is (type 'A'): ")
 
 bot = DLM()
