8463a8e42c5dfe8889bd4723f420a0c0af1c5fcd
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-03-30 09:29:57 -0700

    Update DLM_test.py

--- DLM_test.py
+++ DLM_test.py
@@ -2,19 +2,20 @@ from DLM import DLM
 
 canContinue = True
 
+# introduction and disclaimers
 print(f"{'\033[31m'}Welcome to Dynamic Learning Model Bot (DLM Bot). This bot can be trained for any purposes.{'\033[0m'}")
 print(f"{'\033[31m'}DLM Bot gets smarter for every query asked because it either knows it or learns it for next time{'\033[0m'}")
 print(f"{'\033[31m'}NOTICE: DLM Bot may sometimes misinterpret input or provide inaccurate responses. Please verify important information independently.{'\033[0m'}")
 userChoice = input("\nThere are two options: Train the DLM Bot (type 'T') or use it as is (type 'A'): ")
 
-bot = DLM()
+bot = DLM("stored_data.txt")  # stored_data.txt is the knowledge base that bot will be using
 
-while (userChoice.lower() != "t" and userChoice.lower() != "a"):
+while (userChoice.lower() != "t" and userChoice.lower() != "a"):  # if userChoice is not the expected response, keep asking until it is
     userChoice = input("\nPlease type either 'T' or 'A' to proceed: ")
 
 while canContinue:
     bot.ask(userChoice.lower() == "t")
     choice = input("Continue (Y/N): ")
-    while (choice.lower() != "y" and choice.lower() != "n"):
+    while (choice.lower() != "y" and choice.lower() != "n"):  # if choice is not the expected response, keep asking until it is
         choice = input("\nPlease type either 'Y' or 'N' to proceed: ")
     canContinue = choice.lower() == "y"
