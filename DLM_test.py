cb0e58b4288fe4078c957245fce3090bf6e6a04b
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-05-08 21:50:55 -0700

    May Update - Added Safekey for Training Mode

--- DLM_test.py
+++ DLM_test.py
@@ -1,6 +1,13 @@
 from DLM import DLM
+import time
+
+def loadingAnimation(input):
+    for seconds in range(0, 3):
+        print(f"{input}{'.' * (seconds + 1)}   ", end = " ")
+        time.sleep(0.5)
 
 canContinue = True
+devPassword = "12345"
 
 # introduction and disclaimers
 print(f"{'\033[31m'}Welcome to Dynamic Learning Model Bot (DLM Bot). This bot can be trained for any purposes.{'\033[0m'}")
@@ -8,10 +15,26 @@ print(f"{'\033[31m'}DLM Bot gets smarter for every query asked because it either
 print(f"{'\033[31m'}NOTICE: DLM Bot may sometimes misinterpret input or provide inaccurate responses. Please verify important information independently.{'\033[0m'}")
 userChoice = input("\nThere are two options: Train the DLM Bot (type 'T') or use it as is (type 'A'): ")
 
-bot = DLM("stored_data.txt")  # stored_data.txt is the knowledge base that bot will be using
-
 while (userChoice.lower() != "t" and userChoice.lower() != "a"):  # if userChoice is not the expected response, keep asking until it is
     userChoice = input("\nPlease type either 'T' or 'A' to proceed: ")
+
+password = None
+
+if (userChoice.lower() == "t"):
+    password = input("Enter the password to enter Training Mode: ")
+    while (password != devPassword):
+        password = input("Password is incorrect, try again or type 'stop' to enter in commercial mode instead: ")
+        if (password.lower() == "stop"):
+            userChoice = "A"
+            break
+
+if (password == devPassword):
+    loadingAnimation("Logging in as Trainer")
+else:
+    loadingAnimation("Logging in as Commercial User")
+
+print("\n")
+bot = DLM("stored_data.txt")  # stored_data.txt is the knowledge base that bot will be using
 
 while canContinue:
     bot.ask(userChoice.lower() == "t")
