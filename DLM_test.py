bb1019d737dee2508d5f446e8ba8998b5edc6c80
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-05-28 12:30:00 -0700

    embedded login verification inside DLM instead of test.py

--- DLM_test.py
+++ DLM_test.py
@@ -13,7 +13,6 @@ def loadingAnimation(input):
     print()
 
 canContinue = True
-trainingPwd = "371507"
 
 # introduction and disclaimers
 print(f"{'\033[31m'}Welcome to Dynamic Learning Model Bot (DLM Bot). This bot can be trained for any purposes.{'\033[0m'}")
@@ -23,39 +22,11 @@ userChoice = input("\nThere are two options: Train the DLM Bot (type 'T') or use
 while (userChoice.lower() != "t" and userChoice.lower() != "a"):  # if userChoice is not the expected response, keep asking until it is
     userChoice = input("\nPlease type either 'T' or 'A' to proceed: ")
 
-password = None
-
-if (userChoice.lower() == "t"):
-    password = input("Enter the password to enter Training Mode: ")
-    while (password != trainingPwd):
-        password = input("Password is incorrect, try again or type 'stop' to enter in commercial mode instead: ")
-        if (password.lower() == "stop"):
-            userChoice = "A"
-            break
-
-if (password == trainingPwd):
-    # trainers must understand these rules as DLM can generate bad responses if these instructions are neglected
-    print(f"\n\n{'\033[31m'}MAKE SURE TO UNDERSTAND THE FOLLOWING ANSWER FORMAT EXPECTED FOR EACH CATEGORY FOR THE BOT TO LEARN ACCURATELY:{'\033[0m'}\n")
-    print("*'yesno': Make sure to start your answer responses with \"yes\" or \"no\" ONLY")
-    print("*'process': Each answer must have three steps for your responses, separated by \";\" (semicolon)")
-    print("*'definition': Make sure to not mention the WORD/PHRASE to be defined & always start your response here with \"the\" only")
-    print("*'deadline': Only include the deadline date, as an example, \"March 31st 2025\"")
-    print("*'location': Mention the location only, nothing else. For example, \"The FAFSA.Gov website\"")
-    print("*'generic': Format doesn't matter for this, give your answer in any comprehensive format")
-    print("*'eligibility': Make sure to ONLY start the response with a pronoun like \"you\", \"they\", \"he\", \"she\", etc\n\n")
-
-    confirmation = input("Make sure to understand and note these instructions somewhere as the generated responses would get corrupt otherwise.\nType 'Y' to continue: ")
-    while confirmation.lower() != "y": # trainers must understand the instructions above
-        confirmation = input("You cannot proceed to train without understanding the instructions aforementioned. Type 'Y' to continue: ")
-    loadingAnimation("Logging in as Trainer")
-else:
-    loadingAnimation("Logging in as Commercial User")
-
-print("\n")
 bot = DLM()  # SQL dlm_knowledge.db is the knowledge base that bot will be using
+print("\n")
 
 while canContinue:
-    bot.ask(password)
+    bot.ask(userChoice)
     choice = input("Continue (Y/N): ")
     while (choice.lower() != "y" and choice.lower() != "n"):  # if choice is not the expected response, keep asking until it is
         choice = input("\nPlease type either 'Y' or 'N' to proceed: ")
