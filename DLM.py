c960d912882fa9a8a146887a2ec092b247131b1b
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-06-02 22:28:30 -0700

    added "half" as w2n

--- DLM.py
+++ DLM.py
@@ -451,7 +451,7 @@ class DLM:
             num_mentioned.append(float(match.group(0)).__str__())
 
         # additionally, "double" "triple" "quadruple" also count as numbers, in addition to text numbers (e.g. "three")
-        text_nums = ["double", "triple", "quadruple"]
+        text_nums = ["half", "double", "triple", "quadruple"]
         for match in filtered_query.lower().split():
             try:
                 num = w2n.word_to_num(match)
@@ -468,6 +468,8 @@ class DLM:
                         num_mentioned.append(float(2).__str__())
                     elif t == "triple":
                         num_mentioned.append(float(3).__str__())
+                    elif t == "half":
+                        num_mentioned.append(float(0.5).__str__())
                     else:
                         num_mentioned.append(float(4).__str__())
 
