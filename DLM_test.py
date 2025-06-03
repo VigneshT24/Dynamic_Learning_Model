6c7d7d650687c2eeadc60d553594bbaf8bc415cc
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-06-02 22:21:24 -0700

    extra advisory on CoT

--- DLM_test.py
+++ DLM_test.py
@@ -1,10 +1,12 @@
 from DLM import DLM
 import time
-import threading, itertools, sys # std-lib helpers for the spinner
+import threading, itertools, sys  # std-lib helpers for the spinner
+
 
 # ANSI escape for moving the cursor up N lines
 def move_cursor_up(lines):
     print(f"\033[{lines}A", end="")
+
 
 # loading animation WITH threading for parallel processing
 def loadingAnimation(message, stop_event):
@@ -19,10 +21,13 @@ canContinue = True
 
 # introduction and disclaimers
 print(f"\033[31mWelcome to Dynamic Learning Model Bot (DLM Bot). This bot can be trained for any purposes.\033[0m")
-print(f"\033[31mDLM Bot gets smarter for every query asked because it either knows it or learns it for next time\033[0m")
-print(f"\033[31mNOTICE: DLM Bot may sometimes misinterpret input or provide inaccurate responses. Please verify important information independently.\033[0m")
+print(
+    f"\033[31mDLM Bot gets smarter for every query asked because it either knows it or learns it for next time\033[0m")
+print(
+    f"\033[31mNOTICE: DLM Bot may sometimes misinterpret input or provide inaccurate responses. Please verify important information independently.\033[0m")
 
-userChoice = input("\nThere are three options: Train the DLM Bot (type 'T'), use it commercially (type 'C'), OR try the new experimental arithmetic & conversion solver (type 'E'): ")
+userChoice = input(
+    "\nThere are three options: Train the DLM Bot (type 'T'), use it commercially (type 'C'), OR try the new experimental arithmetic & conversion solver (type 'E'): ")
 while userChoice.lower() not in ("t", "c", "e"):
     userChoice = input("\nPlease type either 'T', 'C', or 'E' to proceed: ")
 
@@ -36,12 +41,13 @@ spinner.start()
 
 bot = DLM()
 
-stop_spinner.set() # signal the spinner to finish
-spinner.join() # wait until it cleans the line
+stop_spinner.set()  # signal the spinner to finish
+spinner.join()  # wait until it cleans the line
 print("SQL Server ready!\n")
 
 if (userChoice.lower() == "e"):
     print("As of now, the CoT can perform non-ambiguous arithmetic and conversion word problems. More development coming soon.")
+    print("Be aware that the bot can make mistakes, even if it is silly.")
     
 while canContinue:
     bot.ask(userChoice)
