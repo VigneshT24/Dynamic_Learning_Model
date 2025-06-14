51346ef3cb2df74a45eed3f4b49dfed981987da2
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-06-14 11:33:59 -0700

    increment version for duplicate operand prevention update

--- dlm-ai/setup.py
+++ dlm-ai/setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='dynamic-learning-model',
-    version='1.2',
+    version='1.2.5',
     author='Vignesh Thondikulam',
     author_email='vignesh.tho2006@gmail.com',
     description='A Dynamic Learning Model for processing NLP queries using hybrid AI and reasoning.',
