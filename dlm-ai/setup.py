6d998e85c102046261765e6422959ca2a83b0998
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-06-18 16:37:39 -0700

    changed name back to original

--- dlm-ai/setup.py
+++ dlm-ai/setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='dlm-ai',
+    name='dynamic-learning-model',
     version='1.3',
     author='Vignesh Thondikulam',
     author_email='vignesh.tho2006@gmail.com',
