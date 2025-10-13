cc6d30360646bda5cced85c6d3cfcd20099e3b2f
Vignesh Thondikulam <176424390+VigneshT24@users.noreply.github.com>
2025-10-12 21:54:19 -0700

    updated readme

--- dlm-ai/setup.py
+++ dlm-ai/setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='dynamic-learning-model',
-    version='3.3.4',
+    version='3.3.5',
     author='Vignesh Thondikulam',
     author_email='vignesh.tho2006@gmail.com',
     description='A Dynamic Learning Model for processing NLP queries using hybrid AI and reasoning.',
