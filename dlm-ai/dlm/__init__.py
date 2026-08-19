ab3078fa0deb6fcb687a420cee8156ec4c9fed3b
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2026-08-18 17:10:57 -0700

    removed redundant spacy download in init py and updated toml file for version and added click

--- dlm-ai/dlm/__init__.py
+++ dlm-ai/dlm/__init__.py
@@ -1,12 +1,1 @@
-import spacy
-from spacy.cli.download import download as spacy_download
-
-def _ensure_spacy_model(model_name="en_core_web_lg"):
-    try:
-        spacy.load(model_name)
-    except OSError:
-        spacy_download(model_name)
-        
-_ensure_spacy_model()
-
 from .DLM import DLM
